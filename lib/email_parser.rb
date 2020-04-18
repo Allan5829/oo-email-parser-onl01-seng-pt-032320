cf3c6e8f85afad5da15cbe81f260586eb17e291c
Allan Farca <allan.farca@gmail.com>
2020-04-18 03:48:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,6 @@ class EmailAddressParser
     split_emails = []
     parsed_emails = []
     split_emails << @email_addresses.split(", ")
-    split_emails << @email_addresses.split(" ")
     split_emails.each do |email|
       if !(parsed_emails.include? (email))
         parsed_emails << email

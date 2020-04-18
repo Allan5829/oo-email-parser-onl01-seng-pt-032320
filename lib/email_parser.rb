ca5e92a25ed295a7ead3803894fa27e3e66433b5
Allan Farca <allan.farca@gmail.com>
2020-04-18 04:09:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,27 +11,7 @@ class EmailAddressParser
   end 
   
   def parse
-    split_emails = []
-    temp_array = []
-    temp_array2 = []
-    parsed_emails = []
-    temp_array << @email_addresses.split(", ")
-    temp_array << @email_addresses.split(" ")
-    temp_array.each do |email|
-      if (email.include? (", "))
-        temp_array2 << email.split(", ")
-      elsif (email.include? (" "))
-        temp_array2 << email.split(" ")
-      else 
-        temp_array2 << email
-      end 
-    end 
-    temp_array2.each do |email|
-      if (!parsed_emails.include? (email))
-        parsed_emails << email
-      end 
-    end 
-    parsed_emails
+    
   end 
   
 end 
